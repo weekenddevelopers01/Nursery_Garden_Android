53824b98ea29adccd5f99f57e10def2d3cb318cb
Rajesh <52477727+weekenddevelopers01@users.noreply.github.com>
2022-05-16 10:07:37 +0530

    initial error handling

--- app/src/main/java/com/example/nurserygardenandroid/ui/activity/OrderDetailActivity.kt
+++ app/src/main/java/com/example/nurserygardenandroid/ui/activity/OrderDetailActivity.kt
@@ -27,7 +27,6 @@ class OrderDetailActivity : BaseActivity() {
 
         setView()
 
-        Toast.makeText(this, ""+intent.getStringExtra(Constants.EXTRA_ORDER_ID), Toast.LENGTH_SHORT).show()
     }
 
     private fun setView() {
